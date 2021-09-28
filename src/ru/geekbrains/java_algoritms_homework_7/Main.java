ba9e4c4c1666620e7cdf786aff86c4781c3fae5b
lekoal <coreglus@gmail.com>
2021-09-28 20:09:54 +0300

    Tasks completed

--- src/ru/geekbrains/java_algoritms_homework_7/Main.java
+++ src/ru/geekbrains/java_algoritms_homework_7/Main.java
@@ -4,5 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Graph graph = new Graph(10);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 6);
+        graph.addEdge(3, 6);
+        graph.addEdge(3, 7);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 8);
+        graph.addEdge(5, 9);
+        graph.addEdge(6, 7);
+        graph.addEdge(7, 9);
+        graph.addEdge(8, 9);
+
+//        System.out.println(graph.getVertexCount());
+//        System.out.println(graph.getEdgeCount());
+//        System.out.println(graph.getAdjList(4));
+
+//        DepthFirstPath dfp = new DepthFirstPath(graph, 2);
+//        System.out.println(dfp.hasPathTo(0));
+//        System.out.println(dfp.pathTo(0));
+
+        BreadthFirstPath bfp = new BreadthFirstPath(graph, 5);
+        System.out.println(bfp.hasPathTo(6));
+        System.out.println(bfp.pathTo(6));
     }
 }
